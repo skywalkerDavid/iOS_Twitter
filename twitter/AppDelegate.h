6b58aa2d0d910c2335a7f9636ba839c27d09b386
davwang <davwang@groupon.com>
2015-11-08 17:12:45 -0800

    initial commit

---
+++ twitter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  twitter
+//
+//  Created by David Wang on 11/8/15.
+//  Copyright © 2015 David Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ twitter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  twitter
+//
+//  Created by David Wang on 11/8/15.
+//  Copyright © 2015 David Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
